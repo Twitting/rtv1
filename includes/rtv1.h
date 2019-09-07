7aa3db064095cbbc904c0616263817cb06b4b7a1
Torchbugs Witting <twitting@ox-p3.21-school.ru>
2019-09-07 17:03:55 +0300

    something

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -6,7 +6,7 @@
 /*   By: twitting <twitting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 16:09:32 by twitting          #+#    #+#             */
-/*   Updated: 2019/09/07 16:10:58 by twitting         ###   ########.fr       */
+/*   Updated: 2019/09/07 17:03:20 by twitting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,24 @@
 
 // # define WINSIZE 900
 
-// typedef struct	s_img
-// {
-// 	void		*img_ptr;
-// 	int			*data;
-// 	int			size_l;
-// 	int			bpp;
-// 	int			endian;
-// }				t_img;
+typedef struct      s_vec
+{
+    double      x;
+    double      y;
+    double      z;
+    double      r;
+    double      g;
+    double      b;
+    double      length;
+}                   t_vec;
 
-// void			ft_error(int errnum, t_fract *f);
-// void			set_color(t_fract *f);
-// void			fractol(t_fract *f);
-// int				mousemove(int x, int y, void *f);
+t_vec v_plus(const *t_vec v1, const *t_vec v2);
+t_vec v_minus(const *t_vec v1, const *t_vec v2);
+t_vec v_mult(const *t_vec v1, const *t_vec v2);
+t_vec v_div(const *t_vec v1, const *t_vec v2);
+*t_vec v_multNum(const *t_vec v, float t);
+*t_vec v_divNum(const *t_vec v, float t);
+float dot(const *t_vec v1, const *t_vec v2);
+t_vec cross(const *t_vec v1, const *t_vec v2);
 
 #endif
---
+++ src/vecOps1.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vecOps1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: twitting <twitting@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 16:50:26 by twitting          #+#    #+#             */
+/*   Updated: 2019/09/07 16:50:52 by twitting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+t_vec v_plus(const *t_vec v1, const *t_vec v2)
+{
+	t_vec result;
+
+	result.x = v1->x + v2->x;
+	result.y = v1->y + v2->y;
+	result.z = v1->z + v2->z;
+	return result;
+}
+
+t_vec v_minus(const *t_vec v1, const *t_vec v2)
+{
+	t_vec result;
+
+	result.x = v1->x - v2->x;
+	result.y = v1->y - v2->y;
+	result.z = v1->z - v2->z;
+	return result;
+}
+
+t_vec v_mult(const *t_vec v1, const *t_vec v2)
+{
+	t_vec result;
+
+	result.x = v1->x * v2->x;
+	result.y = v1->y * v2->y;
+	result.z = v1->z * v2->z;
+	return result;
+}
+
+t_vec v_div(const *t_vec v1, const *t_vec v2)
+{
+	t_vec result;
+
+	result.x = v1->x / v2->x;
+	result.y = v1->y / v2->y;
+	result.z = v1->z / v2->z;
+	return result;
+}
